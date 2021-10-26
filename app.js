b99bd33fe6e5bb2e01db0f71bdfc8c798d67e85a
rudydelorenzo <rdelorenzo5@gmail.com>
2021-10-26 11:53:09 -0600

    Added electron-forge

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600
-    })
+    });
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
     //win.webContents.openDevTools();
 }
 
 app.on('window-all-closed', function () {
-    app.quit()
+    app.quit();
 })
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
 })
